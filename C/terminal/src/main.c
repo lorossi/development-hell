bf7c3aa19c1fbcf266f64284f2417fa45db1267e
lorossi <contact@lorenzoros.si>
2021-10-19 08:52:40 +0200

    added examples

--- C/terminal/src/main.c
+++ C/terminal/src/main.c
@@ -4,38 +4,148 @@ void test_colors()
 {
   reset_styles();
   clear_terminal();
-  /* test foreground colors */
-  for (int i = 0; i < 8; i++)
-  {
-    move_cursor_to(0, i);
-    set_fg(30 + i);
-    printf("Color #%i", i);
-  }
 
-  for (int i = 0; i < 8; i++)
-  {
-    move_cursor_to(10, i);
-    set_fg(90 + i);
-    printf("Color #%i (bright)", i);
-  }
+  /* test foreground colors */
+  move_cursor_to(0, 0);
+  set_fg(fg_BLACK);
+  printf("Color: black");
+
+  move_cursor_to(0, 1);
+  set_fg(fg_RED);
+  printf("Color: red");
+
+  move_cursor_to(0, 2);
+  set_fg(fg_GREEN);
+  printf("Color: green");
+
+  move_cursor_to(0, 3);
+  set_fg(fg_YELLOW);
+  printf("Color: yellow");
+
+  move_cursor_to(0, 4);
+  set_fg(fg_BLUE);
+  printf("Color: blue");
+
+  move_cursor_to(0, 5);
+  set_fg(fg_MAGENTA);
+  printf("Color: magenta");
+
+  move_cursor_to(0, 6);
+  set_fg(fg_CYAN);
+  printf("Color: cyan");
+
+  move_cursor_to(0, 7);
+  set_fg(fg_WHITE);
+  printf("Color: white");
+
+  move_cursor_to(0, 8);
+  set_fg(fg_DEFAULT);
+  printf("Color: default");
+
+  move_cursor_to(15, 0);
+  set_fg(fg_BRIGHT_BLACK);
+  printf("Color: bright black");
+
+  move_cursor_to(15, 1);
+  set_fg(fg_BRIGHT_RED);
+  printf("Color: bright red");
+
+  move_cursor_to(15, 2);
+  set_fg(fg_BRIGHT_GREEN);
+  printf("Color: bright green");
+
+  move_cursor_to(15, 3);
+  set_fg(fg_BRIGHT_YELLOW);
+  printf("Color: bright yellow");
+
+  move_cursor_to(15, 4);
+  set_fg(fg_BRIGHT_BLUE);
+  printf("Color: bright blue");
+
+  move_cursor_to(15, 5);
+  set_fg(fg_BRIGHT_MAGENTA);
+  printf("Color: bright magenta");
+
+  move_cursor_to(15, 6);
+  set_fg(fg_BRIGHT_CYAN);
+  printf("Color: bright cyan");
+
+  move_cursor_to(15, 7);
+  set_fg(fg_BRIGHT_WHITE);
+  printf("Color: bright white");
 
   reset_fg();
   /* test background colors */
-  for (int i = 0; i < 8; i++)
-  {
-    move_cursor_to(30, i);
-    set_bg(40 + i);
-    printf("Color #%i", i);
-  }
+  move_cursor_to(0, 9);
+  set_bg(bg_BLACK);
+  printf("Color: black");
 
-  for (int i = 0; i < 8; i++)
-  {
-    move_cursor_to(40, i);
-    set_bg(100 + i);
-    printf("Color #%i (bright)", i);
-  }
+  move_cursor_to(0, 10);
+  set_bg(bg_RED);
+  printf("Color: red");
 
-  move_cursor_to(0, 8);
+  move_cursor_to(0, 11);
+  set_bg(bg_GREEN);
+  printf("Color: green");
+
+  move_cursor_to(0, 12);
+  set_bg(bg_YELLOW);
+  printf("Color: yellow");
+
+  move_cursor_to(0, 13);
+  set_bg(bg_BLUE);
+  printf("Color: blue");
+
+  move_cursor_to(0, 14);
+  set_bg(bg_MAGENTA);
+  printf("Color: magenta");
+
+  move_cursor_to(0, 15);
+  set_bg(bg_CYAN);
+  printf("Color: cyan");
+
+  move_cursor_to(0, 16);
+  set_bg(bg_WHITE);
+  printf("Color: white");
+
+  move_cursor_to(0, 17);
+  set_bg(bg_DEFAULT);
+  printf("Color: default");
+
+  move_cursor_to(15, 9);
+  set_bg(bg_BRIGHT_BLACK);
+  printf("Color: bright black");
+
+  move_cursor_to(15, 10);
+  set_bg(bg_BRIGHT_RED);
+  printf("Color: bright red");
+
+  move_cursor_to(15, 11);
+  set_bg(bg_BRIGHT_GREEN);
+  printf("Color: bright green");
+
+  move_cursor_to(15, 12);
+  set_bg(bg_BRIGHT_YELLOW);
+  printf("Color: bright yellow");
+
+  move_cursor_to(15, 13);
+  set_bg(bg_BRIGHT_BLUE);
+  printf("Color: bright blue");
+
+  move_cursor_to(15, 14);
+  set_bg(bg_BRIGHT_MAGENTA);
+  printf("Color: bright magenta");
+
+  move_cursor_to(15, 15);
+  set_bg(bg_BRIGHT_CYAN);
+  printf("Color: bright cyan");
+
+  move_cursor_to(15, 16);
+  set_bg(bg_BRIGHT_WHITE);
+  printf("Color: bright white");
+
+  printf("\n");
+
   reset_styles();
   return;
 }
@@ -70,8 +180,9 @@ void test_textmodes()
   reset_textmode();
 
   set_textmode(HIDDEN);
-  printf("HIDDEN TEXT\n");
+  printf("HIDDEN TEXT");
   reset_textmode();
+  printf(" (hidden) \n");
 
   set_textmode(STRIKETHROUGH);
   printf("STRIKETHROUGH TEXT\n");
@@ -104,13 +215,16 @@ void test_mixed()
 
 void test_RGB()
 {
+  const int inc = 2;
+
   reset_styles();
   clear_terminal();
   move_cursor_to(0, 0);
 
-  const int inc = 2;
+  set_fg(fg_RED);
+  printf("RED:   ");
 
-  for (int r = 0; r < 255; r += inc)
+  for (int r = 255; r >= 0; r -= inc)
   {
     RGB color = createRGBcolor(r, 0, 0);
     set_fg_RGB(color);
@@ -118,7 +232,11 @@ void test_RGB()
   }
 
   printf("\n");
-  for (int g = 0; g < 255; g += inc)
+
+  set_fg(fg_GREEN);
+  printf("GREEN: ");
+
+  for (int g = 255; g >= 0; g -= inc)
   {
     RGB color = createRGBcolor(0, g, 0);
     set_fg_RGB(color);
@@ -127,39 +245,54 @@ void test_RGB()
 
   printf("\n");
 
-  for (int b = 0; b < 255; b += inc)
+  set_fg(fg_BLUE);
+  printf("BLUE:  ");
+
+  for (int b = 255; b >= 0; b -= inc)
   {
     RGB color = createRGBcolor(0, 0, b);
     set_fg_RGB(color);
     printf("@");
   }
 
-  printf("\n");
-
+  printf("\n\n");
   reset_styles();
 
-  for (int r = 0; r < 255; r += inc)
+  reset_bg();
+  set_fg(fg_RED);
+  printf("RED:   ");
+
+  for (int r = 255; r >= 0; r -= inc)
   {
     RGB color = createRGBcolor(r, 0, 0);
     set_bg_RGB(color);
-    printf("-");
+    printf(" ");
   }
 
   printf("\n");
-  for (int g = 0; g < 255; g += inc)
+
+  reset_bg();
+  set_fg(fg_GREEN);
+  printf("GREEN: ");
+
+  for (int g = 255; g >= 0; g -= inc)
   {
     RGB color = createRGBcolor(0, g, 0);
     set_bg_RGB(color);
-    printf("-");
+    printf(" ");
   }
 
   printf("\n");
 
-  for (int b = 0; b < 255; b += inc)
+  reset_bg();
+  set_fg(fg_BLUE);
+  printf("BLUE:  ");
+
+  for (int b = 255; b >= 0; b -= inc)
   {
     RGB color = createRGBcolor(0, 0, b);
     set_bg_RGB(color);
-    printf("-");
+    printf(" ");
   }
 
   printf("\n");
@@ -169,10 +302,81 @@ void test_RGB()
 
 void test_HSL()
 {
+  clear_terminal();
+  move_cursor_to(0, 0);
+  reset_styles();
+
+  const int inc = 6;
+  int x, y;
+
+  x = 0;
+  y = 0;
+  for (int h = 0; h < 360; h += inc)
+  {
+    for (int s = 100; s >= 0; s -= inc)
+    {
+      HSL color = createHSLcolor(h, s, 50);
+
+      move_cursor_to(x, y);
+      set_fg_HSL(color);
+      printf("@");
+
+      y++;
+    }
+
+    x++;
+    y = 0;
+  }
+
+  reset_styles();
+
+  x = 0;
+  y = 0;
+  for (int h = 0; h < 360; h += inc)
+  {
+    for (int s = 100; s >= 0; s -= inc)
+    {
+      HSL color = createHSLcolor(h, s, 50);
+
+      move_cursor_to(x, y + 100 / inc + 2);
+      set_bg_HSL(color);
+      printf(" ");
+
+      y++;
+    }
+
+    x++;
+    y = 0;
+  }
+
+  printf("\n\n");
+  reset_styles();
 }
 
 void test_hue()
 {
+  clear_terminal();
+  move_cursor_to(0, 0);
+  reset_styles();
+
+  const int inc = 3;
+  int x = 0;
+
+  for (int h = 0; h < 360; h += inc)
+  {
+    RGB color = HUEtoRGB(h);
+    reset_bg();
+    move_cursor_to(x, 0);
+    set_fg_RGB(color);
+    printf("@");
+
+    move_cursor_to(x, 1);
+    set_bg_RGB(color);
+    printf(" ");
+    x++;
+  }
+
+  reset_styles();
 }
 
 int main()
@@ -202,6 +406,5 @@ int main()
   getchar();
 
   clear_terminal();
-
   return 0;
 }
